dda7d2aff08710f9cfa7ca9f829fcb104d882e8b
Spyros Ntouroukis <spyros-ntouroukis@hotmail.com>
2019-06-19 23:02:29 +0300

    go

--- Crawler/jobs.py
+++ Crawler/jobs.py
@@ -1,6 +1,14 @@
-from requests import get
-#url = 'https://www.kariera.gr/θέση-εργασίας/junior-system-administrator-j2y0pt61dz2fbky6yz6?ipath=INTLMRCJ'
-url = 'https://www.kariera.gr/θέση-εργασίας/καθηγητής-πληροφορικής-jdh8gc5w416bjstwcmh'
+# this is a singe job page crawler for kariera gr site
+
+import re
+import requests
+from bs4 import BeautifulSoup
+
+
+
+
+url = 'https://www.kariera.gr/θέση-εργασίας/junior-system-administrator-j2y0pt61dz2fbky6yz6?ipath=INTLMRCJ'
+#url = 'https://www.kariera.gr/θέση-εργασίας/καθηγητής-πληροφορικής-jdh8gc5w416bjstwcmh'
 response = get(url)
 print(response.text[:500])
 
@@ -27,6 +35,9 @@ apasxolisi = html_soup.find_all('div', class_="snapshot")
 news_content = apasxolisi[0].find_all("div", {'class': 'snapshot-item'})
 list=[]
 
+
+
+
 for wrapper in apasxolisi[0].find_all("div", {'class': 'snapshot-item'}):
     list.append(wrapper.text)
     #print(wrapper.text)
@@ -35,7 +46,19 @@ print("Περιοχή:",list[1].strip())
 
 
 perigrafi = html_soup.find('div', class_="bloc")
+list2=[]
+for wrapper in perigrafi.find_all("div",{'id': 'job-description'}):
+    list2.append(wrapper.text)
+#news_content1 = perigrafi.find("div",{'id': 'job-description'})
+#print(news_content.text)
+#print(news_content1.text)
+print(list2[0].replace("\n", ""))
+kodikos =perigrafi.find("div", {'id': 'job-id'})
+for h3_tag in kodikos.find('h3'):
+    h3_tag.replace_with('')
+print(h3_tag ,":", kodikos.text.replace("\n", ""))
+
 news_content = perigrafi.find("h3", {'class': 'pb'})
-news_content1 = perigrafi.find("div", {'class': ''})
-print(news_content.text)
-print(news_content1.text)
+for h3_tag1 in news_content.find_all('h3'):
+    h3_tag1.replace_with('')
+print( news_content.text.replace("\n",""))
