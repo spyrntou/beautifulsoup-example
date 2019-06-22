c9e66dda3a4976dd2db4dd2482911f7f5624ba7c
Spyros Ntouroukis <spyros-ntouroukis@hotmail.com>
2019-06-22 15:39:13 +0300

    Update crawler list of jobs.py

--- Crawler/crawler list of jobs.py
+++ Crawler/crawler list of jobs.py
@@ -14,41 +14,33 @@ import time
 
 print("------------------------ Script just started ------------------------")
 #initializing the page
-url = 'https://www.kariera.gr/θέσεις-εργασίας?q='
+url = 'https://www.kariera.gr/θέσεις-εργασίας'
 q='a'  # change your q here
-response = get(url +str(q))
+response = get(url + '?q=' +str(q))
 #print(response.text[:500])
 html_soup = BeautifulSoup(response.text, 'html.parser')
 #type(html_soup)
-
 #here we removing styles and scripts
 for script in html_soup(["script", "style"]):
     script.extract()
-
 print("------------------------ javascripts and style removed ------------------------")
-
 #here we start crawl the area of content we need
 page_number = html_soup.find_all('div', class_="bloc clear center")
 job = page_number[0].find_all("a")
 #print(job[5].text)
-
 # store to int the value we got from the page , for number of pages from the site
 a= int(str(job[5].text))
 #print(type(a))
 c=[] # we initialize value to store all urls
-
-
 print("------------------------ Q page numbers Crawled, storing pages ------------------------")
 # get all pages
 for page in range(1 ,a+1):
     #sec=0.5
     #time.sleep(sec)
     str(page)
-    c.append(url + str(page) + '&q=' +str(q))    # on c we store all the queried pages
+    c.append(url+ '?pg=' + str(page) + '&q=' +str(q))    # on c we store all the queried pages
     #r = requests.get(url + str(page) + '&q=it')
    # print(r.text)
-
-
-print("------------------------ Printing stored pages ------------------------")
-for i in range(1,a):
+print("------------------------ Store pages & crawl from each page job pages ------------------------")
+for i in range(0,a):
     print(c[i])
