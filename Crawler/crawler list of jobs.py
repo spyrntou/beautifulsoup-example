7aad37bed34a1dc533ccec6e75226a7bef5a5af7
Spyros Ntouroukis <spyros-ntouroukis@hotmail.com>
2019-06-22 15:05:14 +0300

    laste update commit for the project
    page number q crawl done,
    normal page of job crawled

--- Crawler/crawler list of jobs.py
+++ Crawler/crawler list of jobs.py
@@ -1,3 +1,10 @@
+__author__ = "Spyros Ntouroukis"
+__copyright__ = "Copyright 2019, Crawler"
+__version__ = "1.0.1"
+__maintainer__ = "Spyros Ntouroukis"
+__email__ = "spyros-ntouroukis@hotmail.com"
+__script_infos__ = "This is the part to crawl page number from kariera site"
+import requests
 import re
 from requests import get
 import requests
@@ -5,32 +12,43 @@ from bs4 import BeautifulSoup
 import time
 
 
-
-url = 'https://www.kariera.gr/θέσεις-εργασίας?pg='
-response = get(url)
-print(response.text[:500])
+print("------------------------ Script just started ------------------------")
+#initializing the page
+url = 'https://www.kariera.gr/θέσεις-εργασίας?q='
+q='a'  # change your q here
+response = get(url +str(q))
+#print(response.text[:500])
 html_soup = BeautifulSoup(response.text, 'html.parser')
 #type(html_soup)
-for script in html_soup(["script", "style"]):
-    script.extract()#removing styles and scripts
 
+#here we removing styles and scripts
+for script in html_soup(["script", "style"]):
+    script.extract()
+
+print("------------------------ javascripts and style removed ------------------------")
+
+#here we start crawl the area of content we need
+page_number = html_soup.find_all('div', class_="bloc clear center")
+job = page_number[0].find_all("a")
+#print(job[5].text)
+
+# store to int the value we got from the page , for number of pages from the site
+a= int(str(job[5].text))
+#print(type(a))
 c=[] # we initialize value to store all urls
+
+
+print("------------------------ Q page numbers Crawled, storing pages ------------------------")
 # get all pages
-q='it'  # change your q here
-for page in range(1 ,400):
-    sec=0.5
-    time.sleep(sec)
+for page in range(1 ,a+1):
+    #sec=0.5
+    #time.sleep(sec)
     str(page)
     c.append(url + str(page) + '&q=' +str(q))    # on c we store all the queried pages
     #r = requests.get(url + str(page) + '&q=it')
    # print(r.text)
 
-print(c)
 
-#page_number = html_soup.find_all('div', class_="bloc clear center")
-#job = page_number[0].find("span", {'class': 'ellipsis'})
-#for span_tag in job[0].find_all('span'):
- #   span_tag.replace_with('')
-#print("Τίτλος Δουλειάς",job[0].text.strip())
-#print("Εταιρία:", span_tag.text)
-#print(job.text)
+print("------------------------ Printing stored pages ------------------------")
+for i in range(1,a):
+    print(c[i])
--- Crawler/crawler.py
+++ Crawler/crawler.py
@@ -1,5 +1,26 @@
+#this is a simple crawler to crawl all links from page
+
+
 import re
 import requests
 from bs4 import BeautifulSoup
 
 
+# get all web site links
+pages = set()
+
+def get_links(page_url):
+    global pages
+    pattern = re.compile("^(/)")
+    html = requests.get(f"https://www.kariera.gr/θέσεις-εργασίας?q=it").text  # fstrings require Python 3.6+
+    soup = BeautifulSoup(html, "html.parser")
+    for link in soup.find_all("a", href=pattern):
+        if "href" in link.attrs:
+            if link.attrs["href"] not in pages:
+                new_page = link.attrs["href"]
+                print(new_page)
+                pages.add(new_page)
+                get_links(new_page)
+
+
+get_links("")
