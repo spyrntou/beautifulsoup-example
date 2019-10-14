cd91f5d0409d93b4f322b428537bad1f7d0bf4b1
Spyros Ntouroukis <spyros-ntouroukis@hotmail.com>
2019-10-14 19:40:38 +0300

    Update crawler a tag.py

--- Crawler/crawler a tag.py
+++ Crawler/crawler a tag.py
@@ -1,2 +1,31 @@
-#this is for test perposes file only
+__author__ = "Spyros Ntouroukis"
+__version__ = "1.0.1"
+__maintainer__ = "Spyros Ntouroukis"
+__email__ = "spyros-ntouroukis@hotmail.com"
+__script_infos__ = "This is the part to crawl page number from kariera site"
 
+import re
+from requests import get
+import requests
+from bs4 import BeautifulSoup
+import json
+from time import sleep
+import scrapy
+
+
+
+
+print("------------------------ Script just started ------------------------")
+
+class QuotesSpider(scrapy.Spider):
+    name = 'kariera'
+    start_urls = [
+        'https://www.kariera.gr/θέσεις-εργασίας',
+    ]
+
+    def parse(self, response):
+        for quote in response.css('div.job'):
+            yield {
+                'job-title': quote.css('div.text::text').get()
+                    }
+            return quote
